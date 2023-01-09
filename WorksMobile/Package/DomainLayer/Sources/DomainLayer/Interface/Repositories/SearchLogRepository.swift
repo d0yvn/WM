6e92c9bddad60047a07670efbb27cbc1dcc4fe2d
doyun-park <doyun.park@worksmobile.com>
2023-01-09 18:37:56 +0900

    feat: DefaultSearchRepository를 정의한 Protocol 구현

---
+++ WorksMobile/Package/DomainLayer/Sources/DomainLayer/Interface/Repositories/SearchLogRepository.swift
@@ -0,0 +1,15 @@
+//
+//  SearchLogRepository.swift
+//  
+//
+//  Created by USER on 2023/01/09.
+//
+
+import Combine
+import Foundation
+
+public protocol SearchLogRepository {
+    func fetchSearchLog() -> AnyPublisher<[SearchLog], Error>
+    func delete(keyword: String) -> AnyPublisher<[SearchLog], Error>
+    func update(keyword: String) -> AnyPublisher<String, Error>
+}
