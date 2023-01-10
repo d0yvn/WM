8dce6945e4038565c00a59399af913f582af4772
doyun-park <doyun.park@worksmobile.com>
2023-01-10 14:28:42 +0900

    fix: Swift Package에 적용된 Core Data 파일을 불러오지 못하는 버그 수정

--- WorksMobile/Package/DataLayer/Sources/DataLayer/PersistenceStorage/CoreData/CoreDataService.swift.swift
+++ WorksMobile/Package/DataLayer/Sources/DataLayer/PersistenceStorage/CoreData/CoreDataService.swift.swift
@@ -1,43 +1,44 @@
 //
-//  File.swift
+//  CoreDataService.swift
 //  
 //
 //  Created by USER on 2023/01/09.
 //
 import Combine
 import CoreData
+import DomainLayer
 import Foundation
+import Utils
 
 final public class CoreDataService {
     
     public static let shared = CoreDataService()
     
+    private let modelName = "WorksMobile"
+    
     private init() { }
     
     // MARK: - Core Data stack
     private lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "WorksMobile")
+        
+        
+        guard let modelURL = Bundle.module.url(forResource: modelName, withExtension: "momd") else {
+            fatalError("Error loading model from bundle")
+        }
+
+        guard let mom = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Error initializing mom from: \(modelURL)")
+        }
+        
+        let container = NSPersistentContainer(name: modelName, managedObjectModel: mom)
         container.loadPersistentStores(completionHandler: { _, error in
             if let error = error as NSError? {
-                
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
+            //            container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
         })
         return container
     }()
-    
-    // MARK: - Core Data Saving support
-    func saveContext() {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
     
     func fetch<T: NSManagedObject>(request: NSFetchRequest<T>) -> Future<[T], CoreDataError> {
         return Future<[T], CoreDataError> { [weak self] promise in
@@ -78,16 +79,17 @@ extension CoreDataService {
     
     func saveSearchLog(keyword: String, request: NSFetchRequest<SearchLogEntity>) -> Future<String, CoreDataError> {
         return Future<String, CoreDataError> { [weak self] promise in
-            self?.performBackgroundTask { context in
+            guard let self else { return }
+            
+            self.performBackgroundTask { context in
                 do {
-                    var items = try context.fetch(request)
-                    self?.delete(key: keyword, entities: &items, in: context)
+                    try self.cleanUp(keyword: keyword, context: context)
+                    let entitiy = SearchLogEntity(keyword: keyword, context: context)
+                    try context.save()
                     
-                    _ = SearchLogEntity(keyword: keyword, context: context)
-                    self?.removeSearchLog(entities: &items, in: context)
-                    self?.saveContext()
                     return promise(.success(keyword))
                 } catch {
+                    Logger.print("hge")
                     return promise(.failure(CoreDataError.fetchError(error.localizedDescription)))
                 }
             }
@@ -97,6 +99,17 @@ extension CoreDataService {
 
 extension CoreDataService {
     
+    private func cleanUp(keyword: String, context: NSManagedObjectContext) throws {
+        let request: NSFetchRequest = SearchLogEntity.fetchRequest()
+
+        request.sortDescriptors = [NSSortDescriptor(key: #keyPath(SearchLogEntity.latestDate), ascending: false)]
+        
+        var result = try context.fetch(request)
+        Logger.print(result)
+        delete(key: keyword, entities: &result, in: context)
+        removeSearchLog(entities: &result, in: context)
+    }
+    
     private func delete(key: String, entities: inout [SearchLogEntity], in context: NSManagedObjectContext) {
         entities
             .filter { $0.keyword == key }
@@ -105,9 +118,43 @@ extension CoreDataService {
     }
     
     private func removeSearchLog(limit: Int = 30, entities: inout [SearchLogEntity], in context: NSManagedObjectContext) {
-        guard entities.count > limit else { return }
+        guard entities.count > limit else {
+            Logger.print("stored keyword entitiy = \(entities.count)")
+            return
+        }
         
         entities.suffix(entities.count - limit)
             .forEach { context.delete($0) }
     }
+}
+
+// MARK: - Save search result
+extension CoreDataService {
+    //    func saveMovieResult(movie: MovieDTO) -> Future<[Movie], CoreDataError> {
+    //        let request = MovieEntity.fetchRequest()
+    //        request.predicate = NSPredicate(format: "link == %@", movie.link)
+    //
+    //        return self.fetch(request: request)
+    //            .flatMap { entities in
+    //
+    //                self?.performBackgroundTask { context in
+    //                    guard let entitiy = entities.first else {
+    //                        MovieEntity(movie: movie.toModel(), context: context)
+    //
+    //                        self.saveContext()
+    //                        return
+    //                    }
+    //                }
+    //
+    //            })
+    //            .eraseToAnyPublisher()
+    //            .flatMap { entities in
+    //                if entities.isEmpty {
+    //
+    //                }
+    //            }
+    //        return Future<[Movie], CoreDataError> { [weak self] promise in
+    //
+    //        }
+    //    }
 }
