7bbc1786a21975f392cb26c53aec10e821984b7e
doyun-park <doyun.park@worksmobile.com>
2023-01-10 10:34:27 +0900

    remove: 각 모듈의 테스트를 프로젝트 하도록 각 모듈의 Tests 폴더를 삭제

--- WorksMobile/Package/DataLayer/Package.swift
+++ WorksMobile/Package/DataLayer/Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "DataLayer",
     platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DataLayer",
             targets: ["DataLayer"])
@@ -17,13 +16,8 @@ let package = Package(
         .package(path: "../DomainLayer")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DataLayer",
-            dependencies: ["Alamofire", "DomainLayer"]),
-        .testTarget(
-            name: "DataLayerTests",
-            dependencies: ["DataLayer"])
+            dependencies: ["Alamofire", "DomainLayer"])
     ]
 )
