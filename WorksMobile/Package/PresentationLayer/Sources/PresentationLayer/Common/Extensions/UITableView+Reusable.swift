35b9bfa9bd572b1974b237e20269a6b238037829
doyun-park <doyun.park@worksmobile.com>
2023-01-12 22:39:16 +0900

    feat: TableHeaderFooterView reuseIdentifier, dequeue하는 기능 Extension 구현

--- WorksMobile/Package/PresentationLayer/Sources/PresentationLayer/Common/Extensions/UITableView+Reusable.swift
+++ WorksMobile/Package/PresentationLayer/Sources/PresentationLayer/Common/Extensions/UITableView+Reusable.swift
@@ -13,12 +13,27 @@ extension UITableView {
         self.register(cellClass.self, forCellReuseIdentifier: cellClass.reuseIdentifier)
     }
     
+    func register<T>(_ cellClass: T.Type) where T: UITableViewHeaderFooterView {
+        self.register(cellClass.self, forHeaderFooterViewReuseIdentifier: cellClass.reuseIdentifier)
+    }
+    
     func dequeueCell<T>(_ cellClass: T.Type, for indexPath: IndexPath) -> T? where T: UITableViewCell {
         return self.dequeueReusableCell(withIdentifier: cellClass.reuseIdentifier, for: indexPath) as? T
+    }
+    
+    func dequeueHeaderFooterView<T>(_ cellClass: T.Type) -> T? where T: UITableViewHeaderFooterView {
+        return self.dequeueReusableHeaderFooterView(withIdentifier: cellClass.reuseIdentifier) as? T
     }
 }
 
 extension UITableViewCell {
+    
+    static var reuseIdentifier: String {
+        return String(describing: Self.self)
+    }
+}
+
+extension UITableViewHeaderFooterView {
     
     static var reuseIdentifier: String {
         return String(describing: Self.self)
