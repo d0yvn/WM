1815386946b48399da47a5bd5d59f7f53bd45e38
doyun-park <doyun.park@worksmobile.com>
2023-01-13 11:26:23 +0900

    feat: 버튼 클릭 동작과 TextField동작을 위한 Combine+UIControl Extension 구현

---
+++ WorksMobile/Package/PresentationLayer/Sources/PresentationLayer/Common/Extensions/Publisher+Combine.swift
@@ -0,0 +1,79 @@
+//
+//  Combine+UIControl.swift
+//  
+//
+//  Created by USER on 2023/01/13.
+//
+
+import Combine
+import UIKit
+
+extension UIControl {
+    func controlPublisher(for event: UIControl.Event) -> UIControl.EventPublisher {
+        return UIControl.EventPublisher(control: self, event: event)
+    }
+    
+    struct EventPublisher: Publisher {
+        
+        typealias Output = UIControl
+        typealias Failure = Never
+        
+        let control: UIControl
+        let event: UIControl.Event
+        
+        func receive<S>(subscriber: S) where S: Subscriber, Never == S.Failure, UIControl == S.Input {
+            let subscription = EventSubscription(control: control, subscrier: subscriber, event: event)
+            subscriber.receive(subscription: subscription)
+        }
+    }
+    
+    class EventSubscription<EventSubscriber: Subscriber>: Subscription where EventSubscriber.Input == UIControl, EventSubscriber.Failure == Never {
+        
+        let control: UIControl
+        let event: UIControl.Event
+        var subscriber: EventSubscriber?
+        
+        init(control: UIControl, subscrier: EventSubscriber, event: UIControl.Event) {
+            self.control = control
+            self.subscriber = subscrier
+            self.event = event
+            
+            control.addTarget(self, action: #selector(eventDidOccur), for: event)
+        }
+        
+        func request(_ demand: Subscribers.Demand) {}
+        
+        func cancel() {
+            subscriber = nil
+            control.removeTarget(self, action: #selector(eventDidOccur), for: event)
+        }
+        
+        @objc func eventDidOccur() {
+            _ = subscriber?.receive(control)
+        }
+    }
+}
+
+// MARK: - UIButton
+extension UIButton {
+    var tapPublisher: AnyPublisher<Void, Never> {
+        controlPublisher(for: .touchUpInside)
+            .map { _ in }
+            .eraseToAnyPublisher()
+    }
+}
+
+extension UITextField {
+    var shouldReturnPublisher: AnyPublisher<Void, Never> {
+        controlPublisher(for: .editingDidEndOnExit)
+            .map { _ in }
+            .eraseToAnyPublisher()
+    }
+    
+    var textPublisher: AnyPublisher<String, Never> {
+        controlPublisher(for: .editingChanged)
+            .compactMap { $0 as? UITextField }
+            .compactMap { $0.text }
+            .eraseToAnyPublisher()
+    }
+}
