dcc8c904b3facb975a659b2111d69f99f1efceff
doyun-park <doyun.park@worksmobile.com>
2023-01-16 15:17:22 +0900

    feat: 검색 탭에 대한 case와 title 정의

---
+++ WorksMobile/DomainLayer/Sources/DomainLayer/Models/SearchTab.swift
@@ -0,0 +1,31 @@
+//
+//  SearchTab.swift
+//  
+//
+//  Created by USER on 2023/01/15.
+//
+
+import Foundation
+
+public enum SearchTab: Int, CaseIterable, Hashable {
+    case all = 0
+    case image
+    case blog
+    case movie
+    case webDocument
+
+    public var title: String {
+        switch self {
+        case .all:
+            return "통합"
+        case .image:
+            return "이미지"
+        case .blog:
+            return "블로그"
+        case .movie:
+            return "영화"
+        case .webDocument:
+            return "웹 문서"
+        }
+    }
+}
