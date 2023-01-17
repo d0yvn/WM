9577645df39835d6012630984036cb9f65cdbcd0
doyun-park <doyun.park@worksmobile.com>
2023-01-17 17:32:36 +0900

    refactor: PresentationLayer에서 Model접근가능하도록 Property public으로 변경

--- WorksMobile/DomainLayer/Sources/DomainLayer/Models/Blog.swift
+++ WorksMobile/DomainLayer/Sources/DomainLayer/Models/Blog.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 public struct Blog: Hashable {
-    let title: String
-    let link: String
-    let description: String
-    let bloggerName: String
-    let bloggerLink: String
-    let postDate: String
+    public let title: String
+    public let link: String
+    public let description: String
+    public let bloggerName: String
+    public let bloggerLink: String
+    public let postDate: String
     
     public init(
         title: String,
